d6b9ccc3e3bb87f18cad530d767e460dc003f3f2
Salik <salik.andersen@gmail.com>
2012-11-04 10:22:21 +0100

    new repo

---
+++ src/BinSearch.java
@@ -0,0 +1,12 @@
+
+public class BinSearch {
+  /** find the position of 'target' in presorted array a.
+   * @param a sorted array of integers
+   * @param target the target integer
+   * @param position/index of 'target' in array a, or -1
+   * if 'target' is not in a.
+   */
+  public static int search(int[] a, int target) {
+    return -1;
+  }
+}
